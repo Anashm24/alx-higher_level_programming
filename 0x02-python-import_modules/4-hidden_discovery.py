1d5255712ba737b578c55da8e801a42dff5c2a0b
Anashm24 <anashoumaid19@gmail.com>
2023-11-30 21:46:58 +0100

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 
 if __name__ == "__main__":
     import hidden_4
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
-            print(name)
+            print("{}".format(name))
