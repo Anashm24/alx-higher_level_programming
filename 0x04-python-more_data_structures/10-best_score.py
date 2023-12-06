c392ce4d0328465256d6de6fbcd942750877f324
Anashm24 <anashoumaid19@gmail.com>
2023-12-06 20:58:05 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,18 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    return (max(a_dictionary))
+    best_v = 0
+    best_k = None
+    for key, value in a_dictionary.items():
+        if value >= best_v:
+            best_v = value
+            best_k = key
+    return (best_k)
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
