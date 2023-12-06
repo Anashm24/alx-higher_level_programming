527324a04f2539df4f364f2b3d75e3470426945e
Anashm24 <anashoumaid19@gmail.com>
2023-12-06 19:35:44 +0100

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    a_list = list(a_dictionary.keys())
+    for i in a_list:
+        if i == key:
+            del a_dictionary[key]
+        else:
+            return
