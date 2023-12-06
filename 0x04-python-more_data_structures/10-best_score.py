22bcba7f624eb10588af0ca74fb521f4ad2d13f4
Anashm24 <anashoumaid19@gmail.com>
2023-12-06 20:56:13 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    best_v = 0
-    best_k = None
-    for key, value in a_dictionary.items():
-        if value >= best_v:
-            best_v = value
-            best_k = key
-    return (best_k)
+    return (max(a_dictionary))
