513e1a30323f8ae8f0ecbcaecc4374d6418a4615
Anashm24 <anashoumaid19@gmail.com>
2023-12-18 22:26:34 +0100

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-try:
-    def safe_print_list(my_list=[], x=0):
-        index = 0
-        for i, value in enumerate(my_list, start=1):
+def safe_print_list(my_list=[], x=0):
+    index = 0
+    for i, value in enumerate(my_list, start=1):
+        try:
             if i <= x:
                 print(value, end="")
                 index = i
-        print()
-        return index
-
-except ValueError:
-    print("error")
+        except IndexError:
+            break
+    print()
+    return index
