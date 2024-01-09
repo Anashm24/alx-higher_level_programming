e24f691dbcae1ffcaf322ab88bb2caed8910f6ae
Anashm24 <anashoumaid19@gmail.com>
2024-01-09 14:54:49 +0100

    new

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,9 @@
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+    Returns: the number of characters written"""
+    
     with open(filename, "w", encoding="utf-8") as file:
         wr_file = file.write(text)
     return wr_file
