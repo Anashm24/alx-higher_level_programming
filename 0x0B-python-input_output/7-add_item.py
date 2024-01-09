85524aed1f343238b3bb9a4292b4c9dad81f64fe
Anashm24 <anashoumaid19@gmail.com>
2024-01-09 16:01:00 +0100

    new

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines a script that adds all arguments then save them"""
+import json
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
