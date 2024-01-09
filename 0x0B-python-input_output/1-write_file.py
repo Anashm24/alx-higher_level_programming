1ec691b24c28bace7e664b347cc57522ca8b5766
Anashm24 <anashoumaid19@gmail.com>
2024-01-09 14:48:33 +0100

    new

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defines a function write_file(filename="", text="")"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
