e2464f5335947b4e836e3ce3b68bbb544655d2e4
Anashm24 <anashoumaid19@gmail.com>
2024-01-14 16:39:18 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,45 +1,68 @@
-#!/usr/bin/python
-"""defines a class Rectangle"""
+#!/usr/bin/python3
+"""defines a Rectangle class"""
 from base import Base
 
 
 class Rectangle(Base):
     """a class that inherits from base"""
+    
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle."""
+        
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
+        """get the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
-
+ 
     @property
     def height(self):
+        """get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
-
+        
     @property
     def y(self):
+        """get the x coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
     def y(self, value):
+        if isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
