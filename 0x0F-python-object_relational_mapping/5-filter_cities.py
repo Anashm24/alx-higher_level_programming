402217d70aecf9b05584f4b619c8f3133f398144
Anashm24 <anashoumaid19@gmail.com>
2024-03-17 19:06:08 +0000

    new

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,59 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
 
+"""
+Script that takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa.
+Parameters for script: mysql username, mysql password, database name and
+state name.
+Must use the `MySQLdb` module.
+Script should connect to a MySQL server runnimg on `localhost` at port `3306`
+Results must be in ascending order by `cities.id`.
+Can only use `execute()` once
+Code should not be executed when imported.
+"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
-    cur.close()
+
+    # add check to see if number of arguments is correct
+    if len(argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(
+            argv[0]))
+        exit()
+
+    # establishing a secure connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # createing a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # using parameters to safely execute the query and avoid SQL injection
+    city_query = """SELECT cities.name
+                    FROM states
+                    INNER JOIN cities ON states.id = cities.state_id
+                    WHERE states.name = %s
+                    ORDER BY cities.id ASC"""
+    cursor.execute(city_query, (argv[4],))
+
+    # fetching all the results
+    cities = cursor.fetchall()
+
+    # Displaying the results in the requested format
+    city_names = [city[0] for city in cities]
+    print(", ".join(city_names))
+    """
+    # display/print them out
+    for city in cities:
+        print(city)"""
+
+    # closing the cursor and database connection
+    cursor.close()
     db.close()
