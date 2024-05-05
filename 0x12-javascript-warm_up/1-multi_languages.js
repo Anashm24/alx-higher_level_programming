7d585c1132a1600dccfbab6eb42d24d24c232ded
Anashm24 <anashoumaid19@gmail.com>
2024-05-05 13:40:36 +0100

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// using let to declare variables
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+// print 3 lines
+console.log(line1);
+console.log(line2);
+console.log(line3);
