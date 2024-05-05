46dd312ef116c6c6a021a11e8d7003357c620e33
Anashm24 <anashoumaid19@gmail.com>
2024-05-05 15:04:00 +0100

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// a script that prints 3 lines  using an array of string and a loop
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(`${languages[i]}`);
+}
