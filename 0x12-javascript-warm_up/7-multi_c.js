9a7489569b9a4846a6b42bc394eb1dd067396fd6
Anashm24 <anashoumaid19@gmail.com>
2024-05-05 15:15:31 +0100

    javascript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//  prints x times “C is fun”
+const count = process.argv[2];
+if (count) {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
